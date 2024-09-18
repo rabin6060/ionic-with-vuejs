9803d91d4ac6ae53d64b6736169ffb4c0e84c543
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-18 17:51:18 +0545

    added MemoryPage and MemoryDetail pages.

---
+++ src/pages/MemoryDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <base-layout page-title="Detail Page" back-to-memory-page='/memories'>
+        <h2>working</h2>
+    </base-layout>
+</template>
+
+<script>
+import BaseLayout from '@/components/base/BaseLayout.vue'
+
+export default {
+    components:{
+        BaseLayout
+    }
+}
+</script>
---
+++ src/pages/MemoryPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <base-layout page-title="All Memory">
+            <ion-list>
+                <ion-item v-for="memory in memories" :key="memory.id" :router-link='`/memories/${memory.id}`'>
+                 <ion-thumbnail slot="start">
+                     <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+                     
+                 </ion-thumbnail>
+                
+                 
+                 {{memory.title}}
+                </ion-item>
+                
+            </ion-list>
+    </base-layout>
+</template>
+
+<script>
+import {IonList,IonItem,IonImg,IonThumbnail} from "@ionic/vue"
+import BaseLayout from '@/components/base/BaseLayout.vue'
+export default {
+    components:{
+        IonList,
+        IonItem,
+        BaseLayout,
+        IonImg,
+        IonThumbnail,
+       
+        
+    },
+   computed:{
+    memories(){
+        return this.$store.getters.memories
+    }
+   }
+}
+</script>
+
