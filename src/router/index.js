69470f0295c0a543dad085d6232b04d36acfbd73
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-18 13:38:27 +0545

    vue ionic project setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
-const routes: Array<RouteRecordRaw> = [
+
+const routes = [
   {
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+  
 ]
 
 const router = createRouter({
