9db095d7c29d1cfaa23b8ed366061cc4e6d30636
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-19 12:58:28 +0545

    refractoring the memory page and created memory components

---
+++ src/components/memories/MemoryItems.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-item :router-link='`/memories/${memory.id}`'>
+                 <ion-thumbnail slot="start">
+                     <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+                     
+                 </ion-thumbnail>
+                
+                 
+                 {{memory.title}}
+    </ion-item>
+</template>
+<script>
+import {IonItem,IonThumbnail,IonImg} from '@ionic/vue'
+export default {
+    props:['memory'],
+    components:{
+            IonItem,
+            IonImg,
+            IonThumbnail
+    }
+}
+</script>
---
+++ src/components/memories/MemoryList.vue
@@ -0,0 +1,19 @@
+<template>
+    <ion-list v-for="memory in memories" :key="memory.id">
+        <!-- ion items component render from here -->
+        <memory-items :memory="memory" />
+    </ion-list>
+</template>
+
+<script>
+import {IonList} from '@ionic/vue'
+import MemoryItems from './MemoryItems.vue'
+ export default {
+    props:['memories'],
+    components:{
+        IonList,
+        MemoryItems
+    }
+    
+}
+</script>
