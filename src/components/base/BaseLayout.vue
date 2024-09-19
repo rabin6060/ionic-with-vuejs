21b0fa400019c250ae10e265c72df6ddd5c37b5c
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-19 13:01:40 +0545

    minor change

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="primary">
+                <ion-buttons slot="start">
+                    <ion-back-button :default-href="backToMemoryPage"></ion-back-button>
+                </ion-buttons>
+                
+                <ion-title>
+                    {{pageTitle}}
+                </ion-title>
+            </ion-toolbar>
+            
+        </ion-header>
+        <ion-content>
+            <slot/>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {IonPage,IonContent,IonToolbar,IonHeader,IonTitle,IonBackButton,IonButtons} from "@ionic/vue"
+export default {
+    props:['pageTitle','backToMemoryPage'],
+    components:{
+        IonPage,
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonBackButton,
+        IonButtons
+    }
+}
+</script>
+
