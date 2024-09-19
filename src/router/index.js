f7b6ff04527427be2328d05c504b23e39b2749c2
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-19 13:02:34 +0545

    added MemoryDetail page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-
+import MemoryPage from '../pages/MemoryPage.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/memories'
   },
+  {
+    path:'/memories',
+    component:MemoryPage
+  },
+  {
+    path:'/memories/:id',
+    component: () => import("@/pages/MemoryDetail.vue")
+  }
   
 ]
 
