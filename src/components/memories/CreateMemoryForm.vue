7165d5248e28386b3dc0aa579284604e115b8397
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-19 15:21:44 +0545

    added memory form component and its functionality

---
+++ src/components/memories/CreateMemoryForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <form class="ion-padding" @submit.prevent="handleSubmit">
+                 <ion-list>
+            <ion-item>
+                <ion-label position="floating">Title</ion-label>
+                <ion-input v-model="title" type="text" required />
+            </ion-item>
+            <ion-item>
+                <ion-label position="floating">Image</ion-label>
+                <ion-input v-model="image" type="url" required />
+            </ion-item>
+            <ion-item>
+                <ion-label position="floating">Description</ion-label>
+                <ion-textarea v-model="desc" rows="5" required />
+            </ion-item>
+            <ion-button type="submit" expand="full">Save</ion-button>
+        </ion-list>
+        </form>
+</template>
+
+<script>
+import {IonLabel,IonInput,IonTextarea,IonButton} from "@ionic/vue"
+export default {
+    emits:['submit'],
+    data(){
+        return {
+            title:'',
+            desc:'',
+            image:''
+        }
+    },
+    methods:{
+        handleSubmit(){
+            const memoryData = {
+                title:this.title,
+                desc:this.desc,
+                image:this.image
+            }
+            this.$emit('submit',memoryData)
+        }
+    },
+    components:{
+        IonInput,
+        IonLabel,
+        IonTextarea,
+        IonButton
+    }
+}
+</script>
---
+++ src/components/memories/MemoryOverview.vue
@@ -0,0 +1,20 @@
+<template>
+    <ion-img :src = "image"  />
+    <h2 class="ion-text-center">
+        {{title}}
+    </h2>
+    <p class="ion-text-center">
+     {{desc}}
+    </p>
+</template>
+
+<script>
+import {IonImg} from "@ionic/vue"
+export default {
+    props:["title","image","desc"],
+  components:{
+    IonImg,
+ 
+  }
+}
+</script>
