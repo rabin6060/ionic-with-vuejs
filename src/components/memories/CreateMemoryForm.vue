2be065c9fb1d7891b3f74cecb2288e0166e0f601
Rabin Mainali <rabinmainali1999@gmail.com>
2024-09-19 16:13:20 +0545

    camera funcionlaity added

--- src/components/memories/CreateMemoryForm.vue
+++ src/components/memories/CreateMemoryForm.vue
@@ -6,8 +6,14 @@
                 <ion-input v-model="title" type="text" required />
             </ion-item>
             <ion-item>
-                <ion-label position="floating">Image</ion-label>
-                <ion-input v-model="image" type="url" required />
+                <ion-thumbnail slot="start">
+                    <ion-img v-if="image" :src="image" alt="image" />
+                    <ion-img else src="" />
+                </ion-thumbnail>
+                <ion-button type="button" fill="outline" @click="handleCamera">
+                    <ion-icon slot="start" color="primary" :icon="camera" />
+                    Take Photo
+                </ion-button>
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Description</ion-label>
@@ -19,14 +25,17 @@
 </template>
 
 <script>
-import {IonLabel,IonInput,IonTextarea,IonButton} from "@ionic/vue"
+import {IonLabel,IonInput,IonTextarea,IonButton,IonIcon,IonThumbnail,IonImg} from "@ionic/vue"
+import {camera} from "ionicons/icons"
+import {Camera, CameraResultType, CameraSource} from "@capacitor/camera"
 export default {
     emits:['submit'],
     data(){
         return {
             title:'',
             desc:'',
-            image:''
+            image:'',
+            camera
         }
     },
     methods:{
@@ -37,13 +46,26 @@ export default {
                 image:this.image
             }
             this.$emit('submit',memoryData)
-        }
+        },
+        async handleCamera(){
+            const image = await Camera.getPhoto({
+                quality: 90,
+                allowEditing: false,
+                resultType: CameraResultType.Uri, // You can also use Base64 for the image result
+                source: CameraSource.Camera, // You can use CameraSource.Prompt, .Photos, or .Camera
+            });
+
+            this.image = image.webPath; // Set the image path
+                }
     },
     components:{
         IonInput,
         IonLabel,
         IonTextarea,
-        IonButton
+        IonButton,
+        IonIcon,
+        IonThumbnail,
+        IonImg
     }
 }
 </script>
